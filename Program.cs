3b305265960932338b9700add07e42e8540d1595
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-04-26 18:52:24 +0300

    odbc

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿//OdbcConnection 
+﻿using System.Data.Odbc;
+
+string MyConString = "DRIVER={MySQL ODBC 8.0 Unicode Driver};" +
+            "SERVER=localhost;" +
+            "DATABASE=mospolylabdb;" +
+            "UID=root;" +
+            "PASSWORD=root;" +
+            "OPTION=3";
+
+OdbcConnection odbcConnection = new OdbcConnection(MyConString); 
+OdbcCommand command = odbcConnection.CreateCommand();
+command.CommandText = "SELECT * FROM mospolylabdb.users";
+odbcConnection.Open();
+OdbcDataReader odbcDataReader = command.ExecuteReader();
+while (odbcDataReader.Read())
+{
+    Console.WriteLine(odbcDataReader.GetString(0));
+}
